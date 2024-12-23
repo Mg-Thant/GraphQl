d8da8c9a5d21c9a194c46130b24d62f9cb01e534
Mg Thant <tzinwin517@gmail.com>
2024-12-23 15:26:00 +0630

    add login & signup resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { APP_SECRET } = require("../utils");
+
 const post = (parent, args, context) => {
   const { url, description } = args;
   const newLink = context.prisma.link.create({
@@ -10,6 +14,46 @@ const post = (parent, args, context) => {
   return newLink;
 };
 
+const signup = async (parent, args, context) => {
+  const password = await bcrypt.hash(args.password, 10);
+
+  const user = await context.prisma.user.create({
+    data: { ...args, password },
+  });
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+};
+
+const login = async (parent, args, context) => {
+  const user = await context.prisma.userfindUnique({
+    where: { email: args.email },
+  });
+
+  if (!user) {
+    throw new Error("No such user found");
+  }
+
+  const valid = await bcrypt.compare(args.password, user.password);
+
+  if (!valid) {
+    throw new Error("Invalid user credentials");
+  }
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+  return {
+    token,
+    user,
+  };
+};
+
 module.exports = {
   post,
+  login,
+  signup,
 };
