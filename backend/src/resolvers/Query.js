33618efa20a0777798fd6b979fde9fc8e3984e75
Mg Thant <tzinwin517@gmail.com>
2024-12-23 19:29:46 +0630

    feedByCurrentUserId add

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,12 @@ function feed(parent, args, context) {
   return context.prisma.link.findMany();
 }
 
+const feedByCurrentUserId = (parent, args, context) => {
+  const { userId } = context;
+  return context.prisma.user.findUnique({ where: { id: userId } }).links();
+};
+
 module.exports = {
   feed,
+  feedByCurrentUserId,
 };
